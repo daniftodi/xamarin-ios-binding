2c3ba3db6382def2cda091eedc15e27551dd3e60
Dan <daniftodi@gmail.com>
2024-05-20 09:32:54 +0300

    Added built xcframework

---
+++ build/Build/Intermediates.noindex/ArchiveIntermediates/Extole-iOS/IntermediateBuildFilesPath/Extole-iOS.build/Release-iphonesimulator/Extole-iOS.build/Objects-normal/x86_64/Extole_iOS_vers.d
@@ -0,0 +1,2 @@
+dependencies: \
+  /Users/dan/projects/ExtoleForXamariniOS/build/Build/Intermediates.noindex/ArchiveIntermediates/Extole-iOS/IntermediateBuildFilesPath/Extole-iOS.build/Release-iphonesimulator/Extole-iOS.build/DerivedSources/Extole_iOS_vers.c
---
+++ build/Extole-iOS-simulator.xcarchive/Products/Library/Frameworks/Extole_iOS.framework/Headers/Extole_iOS.h
@@ -0,0 +1,18 @@
+//
+//  Extole_iOS.h
+//  Extole-iOS
+//
+//  Created by Puneet on 26/04/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Extole_iOS.
+FOUNDATION_EXPORT double Extole_iOSVersionNumber;
+
+//! Project version string for Extole_iOS.
+FOUNDATION_EXPORT const unsigned char Extole_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Extole_iOS/PublicHeader.h>
+
+
